169d3802917b27ad9841aa44e4307add377e72ec
alyssa horrocks & kendra ash <student@epicodus.com>
2015-12-16 13:34:20 -0800

    Fix binary calculating - actually works.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,12 @@
 function binaryConverter(num) {
   num = parseInt(num);
+
   var binaryResult = [];
   var remainder;
-  var quotient = num / 2;
+  var quotient;
   var counter = 0;
 
-  while (counter < 3) {
+  while (counter < 1) {
 
    remainder = num % 2;
    quotient = Math.floor(num / 2);
@@ -18,5 +19,6 @@ function binaryConverter(num) {
    if (quotient === 0 && remainder === 0)
     { counter ++; }
   }
+
   return parseInt(binaryResult.reverse().join(""));
 }
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,9 @@
 describe("binaryConverter", function() {
   it("will take a decimal number and return it in binary", function() {
     expect(binaryConverter("2")).to.equal(10);
+  }),
+
+  it("will take a decimal number and return it in binary", function() {
+    expect(binaryConverter("5")).to.equal(101);
   });
 });
